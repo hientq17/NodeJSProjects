8e86036be877e91b5f473f5152af862be7ba23cd
Trinh Quang Hien <quanghien1712@gmail.com>
2020-12-04 02:15:39 +0700

    update 04/12/2020

--- Simple-blog/app.js
+++ Simple-blog/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,7 +21,27 @@ const posts = [];
 
 app.get("/",(req,res) => {
   console.log(posts);
-  res.render("home",{content: homeStartingContent, posts: posts});
+  var homePosts = [];
+  posts.forEach((post) => {
+    let newPost = {
+        title: post.title,
+        content: post.content.substring(0,100) + "  . . ."
+    }
+    homePosts.push(newPost);
+  });
+  
+  res.render("home",{content: homeStartingContent, posts: homePosts});
+})
+
+app.get("/post/:postId",(req,res) => {
+  let postId = req.params.postId;
+  var foundPosts = [];
+  posts.forEach((post) => {
+    if(_.lowerCase(post.title)==_.lowerCase(postId)){
+      foundPosts.push(post);
+    }
+    res.render("post",{posts: foundPosts});
+  });
 })
 
 app.get("/about",(req,res) => {
